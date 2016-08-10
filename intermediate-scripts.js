6c63dc0420736c465121c1b9f82fa004dfeded73
VictorGuzman <viaguz@gmail.com>
2016-08-09 22:20:12 -0600

    Add an prime sum algorithm for a given number

--- intermediate-scripts.js
+++ intermediate-scripts.js
@@ -416,3 +416,41 @@ function fibonacci(num) {
 }
 
 sumFibs(75025);
+
+/* Sum All Primes */
+
+
+function sumPrimes(num) {
+  var primeArr = [];
+  for (var i = 1; i <= num; i++) {
+    if (isPrime(i)) {
+      primeArr.push(i);
+    }
+  }
+  return primeArr.reduce(function(a, b){
+    return a + b;
+  });
+}
+
+// Verifies if number is prime
+function isPrime(num) {
+  if (num <= 1) {
+    return false;
+  }
+  else if (num <= 3) {
+    return true;
+  }
+  else if ((num % 2 === 0) || (num % 3 === 0)) {
+    return false;
+  }
+  var i = 5;
+  while ((i * i) <= num) {
+    if ((num % i === 0) || (num % (i + 2) === 0)) {
+      return false;
+    }
+    i += 6;
+  }
+  return true;
+}
+
+sumPrimes(10);
