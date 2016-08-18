037f68a76ab0edf791b2a45ba6c67a0817e3d034
VictorGuzman <viaguz@gmail.com>
2016-08-17 21:22:05 -0600

    Add a binary agent algorithm

--- intermediate-scripts.js
+++ intermediate-scripts.js
@@ -520,3 +520,17 @@ function steamrollArray(arr) {
 }
 
 steamrollArray([1, [2], [3, [[4]]]]);
+
+/* Binary Agents */
+
+function binaryAgent(str) {
+  var binArr = str.split(" ");
+  var resStr = binArr.reduce(function(a, b){
+    var decimalVal = parseInt(b, 2);
+    var charVal = String.fromCharCode(decimalVal);
+    return a + charVal;
+  }, "");
+  return resStr;
+}
+
+binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111");
