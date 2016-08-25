60b698043c60e56b7c07777e9a96f973256c76db
VictorGuzman <viaguz@gmail.com>
2016-08-24 23:21:42 -0600

    Add an exchange calculator cash in drawer algorithm

--- advanced-scripts.js
+++ advanced-scripts.js
@@ -52,3 +52,52 @@ function diffArray(arr1, arr2) {
 }
 
 sym([1, 2, 3], [5, 2, 1, 4]);
+
+/* Exact Change */
+
+var denoms = [
+  { name: "ONE HUNDRED", value: 100.00 },
+  { name: "TWENTY", value: 20.00 },
+  { name: "TEN", value: 10.00 },
+  { name: "FIVE", value: 5.00 },
+  { name: "ONE", value: 1.00 },
+  { name: "QUARTER", value: 0.25 },
+  { name: "DIME", value: 0.10 },
+  { name: "NICKEL", value: 0.05 },
+  { name: "PENNY", value: 0.01 }
+];
+
+function checkCashRegister(price, cash, cid) {
+  var change = cash - price;
+  var totalCid = cid.reduce(function(acc, succ) {
+    return acc += succ[1];
+  },0.0);
+  if (totalCid < change) {
+    return "Insufficient Funds";
+  }
+  else if (totalCid === change) {
+    return "Closed";
+  }
+  else {
+    cid = cid.reverse();
+    var exactChange = denoms.reduce(function(acc, succ, index){
+      if (change >= succ.value) {
+        var changeDiff = 0.0;
+        while (change >= succ.value && cid[index][1] >= succ.value) {
+          changeDiff += succ.value;
+          change -= succ.value;
+          change = Math.round(change * 100) / 100;
+          cid[index][1] -= succ.value;
+        }
+        acc.push([succ.name, changeDiff]);
+        return acc;
+      }
+      else {
+        return acc;
+      }
+    }, []);
+    return exactChange.length > 0 && change === 0 ? exactChange : "Insufficient Funds";
+  }
+}
+
+checkCashRegister(19.50, 20.00, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.10], ["QUARTER", 4.25], ["ONE", 90.00], ["FIVE", 55.00], ["TEN", 20.00], ["TWENTY", 60.00], ["ONE HUNDRED", 100.00]]);
