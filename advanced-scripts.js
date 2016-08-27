ae12e4af0278c27561c39d3510f7b046327ff5f8
VictorGuzman <viaguz@gmail.com>
2016-08-26 22:52:59 -0600

    Add an algorithm that calculates the non-repeated permutations of a string

--- advanced-scripts.js
+++ advanced-scripts.js
@@ -166,3 +166,36 @@ var newInv = [
 ];
 
 updateInventory(curInv, newInv);
+
+/* No repeats please */
+
+function permAlone(str) {
+  var arr = str.split('');
+  var result = 0;
+  var perms = [];
+
+  // Swaps two elements of a given array
+  function swap(a, b) {
+    var tmp = arr[a];
+    arr[a] = arr[b];
+    arr[b] = tmp;
+  }
+
+  // Generates permutations
+  function generate(n){
+    var re = /([a-z])\1+/;
+    if (n === 1 && !re.test(arr.join(''))) {
+      result++;
+    }
+    else {
+      for (var i = 0; i !== n; i++) {
+        generate(n - 1);
+        swap(n % 2 ? 0 : i, n - 1);
+      }
+    }
+  }
+  generate(arr.length);
+  return result;
+}
+
+permAlone('aabb');
